3f586fbaa1aefabbff976e4838ca71f1006f5503
Mykola Kalinchuk <lifeisg00d7@proton.me>
2025-09-09 17:58:01 +0300

    Improve structure

--- dtdiff.py
+++ dtdiff.py
@@ -3,44 +3,49 @@
 import datetime as dt
 import argparse
 
-def datediff(first: str|None = None, second: str|None = None) -> str:
-    today = dt.datetime.now()
-    try:
-        if first is None:
-            return f'{today.strftime("%d.%m.%Y")}'
-        elif first == 'today':
-            first = today
-        elif first.isdigit() or (first.startswith('-') and first[1:].isdigit()):
-            result = today + dt.timedelta(days=int(first))
-            return result.strftime('%d.%m.%Y')
-        else:
-            if len(first.split('.')) == 2:
-                first += '.' + str(today.year)
-            first = dt.datetime.strptime(first, '%d.%m.%Y')
+TODAY = dt.date.today()
 
-        if second is None:
-            diff = (first - today).days + 1
-            return show_days(diff)
-        elif second == 'today':
-            second = today
-        elif second.isdigit() or (second.startswith('-') and second[1:].isdigit()):
-            result = first + dt.timedelta(days=int(second))
-            return result.strftime('%d.%m.%Y')
+
+def parse_date(date_str: str) -> dt.date | int:
+    """Перетворює вхідний рядок на дату або число - кількість днів"""
+    if date_str.isdigit() or (date_str.startswith('-') and date_str[1:].isdigit()):
+        return int(date_str)
+    elif '.' in date_str:
+        if len(date_str.split('.')) == 2:
+            date_str += '.' + str(TODAY.year)
+            return dt.datetime.strptime(date_str, '%d.%m.%Y').date()
+        elif len(date_str.split('.')) == 3:
+            return dt.datetime.strptime(date_str, '%d.%m.%Y').date()
         else:
-            if len(second.split('.')) == 2:
-                second += '.' + str(today.year)
-            second = dt.datetime.strptime(second, '%d.%m.%Y')
-    except (TypeError, ValueError):
-        return (
-            'Неправильний формат. Використовуйте: '
-            'DD.MM, DD.MM.YYYY, "today" або число (дні).'
-        )
-    diff = (second - first).days
-    if first == today or second == today:
-        diff += 1
-    return show_days(diff)
+            raise ValueError('Неправильний формат дати. Використовуйте: DD.MM або DD.MM.YYYY')
+    else:
+        raise ValueError('Неправильний формат. Використовуйте: '
+        'DD.MM, DD.MM.YYYY або число (кількість днів).')
+
+
+def parse_arguments(argv: list):
+    """Парсить аргументи командного рядка"""
+    if len(argv) == 2:
+        return parse_date(argv[0]), parse_date(argv[1])
+    elif len(argv) == 1:
+        return TODAY, parse_date(argv[0])
+    else:
+        raise ValueError('Некоректна кількість аргументів')
+
+
+def date_difference(date1: dt.date, date2: dt.date) -> str:
+    """Обчислює різницю в днях між двома датами"""
+    difference = (date2 - date1).days
+    return show_days(difference)
+
+
+def add_days(date: dt.date, days: int) -> dt.date:
+    """Додає певну кількість днів до дати"""
+    return date + dt.timedelta(days=days)
+
 
 def show_days(diff: int) -> str:
+    """Повертає рядок з правильною формою слова 'день'"""
     if 11 <= diff % 100 <= 14:
         return f'{diff} днів'
     elif diff % 10 == 1:
@@ -50,6 +55,7 @@ def show_days(diff: int) -> str:
     else:
         return f'{diff} днів'
 
+
 def arg_parser():
     parser = argparse.ArgumentParser(description='Обчислення різниці між двома датами або обчислення дати, що настає через певну кількість днів.')
     parser.add_argument('dates',
@@ -58,16 +64,19 @@ def arg_parser():
                         help='Дата у форматі DD.MM або DD.MM.YYYY, "today", або кількість днів (ціле число).')
     return parser.parse_args()
 
+
+def main(first, second):
+    if isinstance(second, int):
+        result_date = add_days(first, second)
+        return f'{result_date.strftime('%d.%m.%Y')}'
+    else:
+        diff = date_difference(first, second)
+        return diff
+
 if __name__ == '__main__':
     try:
         args = arg_parser()
-        if len (args.dates) == 0:
-            print(datediff())
-        elif len(args.dates) == 1:
-            print(datediff(args.dates[0]))
-        elif len(args.dates) == 2:
-            print(datediff(args.dates[0], args.dates[1]))
-        else:
-            print('Забагато аргументів. Вкажіть не більше двох аргументів.')
+        first, second = parse_arguments(args.dates)
+        print(main(first, second))
     except Exception as e:
         print(f'Виникла помилка: {e}')
